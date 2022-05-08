c863be9e01de5a269a0d110c21af1ca0fdd23823
david <david.dehe.pro@gmail.com>
2022-05-08 22:48:27 +0200

    add konvas

--- src/views/main.jsx
+++ src/views/main.jsx
@@ -1,12 +1,20 @@
 import { Game } from "../domain/controller/game";
-import Canvas from "canvas";
+import { Circle, Stage, Layer, Rect, Text } from "react-konva";
+import Konva from "konva";
+
 export const Main = () => {
   const game = new Game();
-  console.table(game.grid);
+  console.table(game);
 
   return (
     <div>
-      <Canvas />
+      <p>test</p>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Text text="Try click on rect" />
+          <Circle x={0} y={0} draggable radius={50} fill={"green"} />
+        </Layer>
+      </Stage>
     </div>
   );
 };
